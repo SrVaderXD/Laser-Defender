174116452e7e90638f7e1627bd2e41f2b125d871
Henrique Loureiro de Faria <henriquelfaria08@gmail.com>
2022-05-03 18:25:45 -0300

    Added boundaries and enemies pathfinder

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfig;
+    List<Transform> wayPoints;
+    int wayPointIndex = 0;
+
+    void Start()
+    {
+        wayPoints = waveConfig.GetWayPoints();
+        transform.position = wayPoints[wayPointIndex].position;
+    }
+
+    void Update()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if(wayPointIndex < wayPoints.Count)
+        {
+            Vector3 targetPosition = wayPoints[wayPointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
+
+            if(transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,39 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
     Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    void Start() 
+    {
+        InitBounds();
+    }
     void Update()
     {
         move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+
     void OnMove(InputValue value) 
     {
-        rawInput = value.Get<Vector2>();
-        Debug.Log(rawInput);
+        rawInput = value.Get<Vector2>();        
     }
 
     void move() {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingRight, maxBounds.x - paddingLeft);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 }
